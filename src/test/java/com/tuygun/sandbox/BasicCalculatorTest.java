0ff16479d1a59c0f58252bddda9676c8b43be100
Serhan Tuygun <serhan.tuygun@ericsson.com>
2019-05-31 17:25:57 +0300

    Nested Test Added
    Change-Id: I0139e5410775acffa3c3d6b1e0f017a58980ed09

--- src/test/java/com/tuygun/sandbox/BasicCalculatorTest.java
+++ src/test/java/com/tuygun/sandbox/BasicCalculatorTest.java
@@ -1,22 +1,58 @@
 package com.tuygun.sandbox;
 
-import com.tuygun.sandbox.BasicCalculator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+@DisplayName("Calculator Test")
 class BasicCalculatorTest {
-    BasicCalculator calculator;
+    private final BasicCalculator calculator = new BasicCalculator();
 
-    @BeforeEach
-    public void init(){
-        calculator = new BasicCalculator();
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
     }
 
-    @Test
-    void testAddFunction(){
-        assertEquals(2.0,
-                calculator.add(1.0, 1.0), "Add functionality is not working");
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+
+    @Nested
+    @DisplayName("Addition Related Tests")
+    class AdditionTest {
+        @Test
+        @DisplayName("Check Calculator Add Functionality")
+        void testAddFunction() {
+            assertEquals(2.0,
+                    calculator.add(1.0, 1.0), "Add functionality is not working");
+        }
+    }
+
+    @Nested
+    @DisplayName("Subtraction Related Tests")
+    class SubtractTest {
+        @Test
+        @DisplayName("Basic Subtract Functionality-1")
+        void testBasicSubtractFunction() {
+            assertEquals(0.0, calculator.subtract(1.0, 1.0));
+        }
+
+        @Test
+        @DisplayName("Basic Subtract Functionality-2")
+        void testSubtractFunction() {
+            assertNotEquals(1.0, calculator.subtract(1.0, 1.0));
+        }
     }
 }
